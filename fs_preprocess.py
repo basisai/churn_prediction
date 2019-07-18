d97e741176ecfe15905a7df9929265f34fb806dd
hfchong <hon_fah@hotmail.com>
2019-07-18 18:09:44 +0800

    Refactor fs_preprocess.py

--- fs_preprocess.py
+++ fs_preprocess.py
@@ -3,15 +3,21 @@ Script to preprocess subscriber data and save to feature store.
 """
 import os
 import time
+
 from bedrock_client.bedrock.feature_store import get_feature_store
-from utils.preprocess import preprocess_subscriber
 from pyspark.sql import SparkSession
+
+from utils.preprocess import preprocess_subscriber
+
 
 SUBSCRIBER_FS = os.getenv("SUBSCRIBER_FS")
 
 
-if __name__ == '__main__':
-    with SparkSession.builder.appName("SubscriberPreprocessing").getOrCreate() as spark:
+def main():
+    """Feature pipeline"""
+    with (SparkSession.builder
+          .appName("SubscriberPreprocessing")
+          .getOrCreate()) as spark:
         spark.sparkContext.setLogLevel("FATAL")
 
         start = time.time()
@@ -24,14 +30,21 @@ if __name__ == '__main__':
             .toPandas()
         )
         print("\tTime taken = {:.2f} min".format((time.time() - start) / 60))
-        print("\tNumber of active subscribers = {}".format(subscriber_pandasdf.shape[0]))
+        print("\tNumber of active subscribers = {}"
+              .format(subscriber_pandasdf.shape[0]))
 
-    fs = get_feature_store()
+    feature_store = get_feature_store()
     start = time.time()
     print("\tWriting active subscribers into fs")
-    fs.write_pandas_df(subscriber_pandasdf, SUBSCRIBER_FS, "User_id")
+    feature_store.write_pandas_df(subscriber_pandasdf,
+                                  SUBSCRIBER_FS,
+                                  "User_id")
     print("\tTime taken = {:.2f} min".format((time.time() - start) / 60))
 
     print("\tTest calling feature store")
-    User_id = "68b1fbe7f16e4ae3024973f12f3cb313"
-    print(fs.read(SUBSCRIBER_FS, [User_id])[User_id])
+    user_id = "68b1fbe7f16e4ae3024973f12f3cb313"
+    print(feature_store.read(SUBSCRIBER_FS, [user_id])[user_id])
+
+
+if __name__ == '__main__':
+    main()
