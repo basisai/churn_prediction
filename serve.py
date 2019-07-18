b1b4491478114640da3a6f27a02e11324eadfaf2
hfchong <hon_fah@hotmail.com>
2019-07-18 19:13:30 +0800

    Refactor serve.py

--- serve.py
+++ serve.py
@@ -1,17 +1,18 @@
 """
 Script for serving.
 """
+from concurrent.futures import ThreadPoolExecutor
 import os
 import logging
 import pickle
 import time
+
+from bedrock_client.bedrock.feature_store import get_feature_store
 import grpc
 import numpy as np
-
 import serve_pb2
 import serve_pb2_grpc
-from bedrock_client.bedrock.feature_store import get_feature_store
-from concurrent.futures import ThreadPoolExecutor
+
 from utils.constants import AREA_CODES, STATES, SUBSCRIBER_FEATURES
 
 SUBSCRIBER_FS = "subscriber_fs"
@@ -22,12 +23,13 @@ _ONE_DAY_IN_SECONDS = 60 * 60 * 24
 
 
 class PredictorService(serve_pb2_grpc.PredictorServicer):
+    """grpc predictor service"""
     def __init__(self):
         self.feature_store = get_feature_store()
         with open(OUTPUT_MODEL_NAME, "rb") as model_file:
             self.model = pickle.load(model_file)
 
-    def predict_prob(self, User_id):
+    def predict_prob(self, user_id):
         """Predict churn probability of user with User_id.
 
         Args:
@@ -38,7 +40,7 @@ class PredictorService(serve_pb2_grpc.PredictorServicer):
         """
         # Get subscriber features from feature store
         subscriber_features = self.feature_store.read(
-            SUBSCRIBER_FS, [User_id])[User_id]
+            SUBSCRIBER_FS, [user_id])[user_id]
 
         row_feats = list()
         for col in SUBSCRIBER_FEATURES:
@@ -57,10 +59,14 @@ class PredictorService(serve_pb2_grpc.PredictorServicer):
                 row_feats.append(0)
 
         # Score
-        churn_prob = self.model.predict_proba(np.array(row_feats).reshape(1, -1))[:, 1].item()
+        churn_prob = (
+            self.model
+            .predict_proba(np.array(row_feats).reshape(1, -1))[:, 1].item()
+        )
         return churn_prob
 
     def PredictProb(self, request, context):
+        # pylint: disable=broad-except,invalid-name
         try:
             churn_prob = self.predict_prob(request.User_id)
             return serve_pb2.PredictResponse(churn_prob=churn_prob)
@@ -68,7 +74,8 @@ class PredictorService(serve_pb2_grpc.PredictorServicer):
             logging.error(e, exc_info=True)
 
 
-if __name__ == "__main__":
+def main():
+    """Start grpc server"""
     server = grpc.server(ThreadPoolExecutor(max_workers=10))
     serve_pb2_grpc.add_PredictorServicer_to_server(PredictorService(), server)
     server.add_insecure_port(f"[::]:{SERVER_PORT}")
@@ -78,3 +85,7 @@ if __name__ == "__main__":
             time.sleep(_ONE_DAY_IN_SECONDS)
     except KeyboardInterrupt:
         server.stop(0)
+
+
+if __name__ == "__main__":
+    main()
